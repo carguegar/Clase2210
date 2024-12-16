c6dfeeb58b737ed9b86d6f49f638e8c855d1ac46
carguegar <carlosgue.2024.alumn@cescristorey.com>
2024-12-16 20:55:59 +0100

    caso avanzado, 30 min.

--- prueba.cpp
+++ prueba.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main () {
 	int numero_casos = 1, equipos = 1, repesca, numero, numero_real, plazas, plazas_totales, j = 0, i=0;
-	char federaciones_sobrantes;
+	bool error_equipos = false, error_simbolo = false, error_federacion1 = false, error_federacion2 = false, error_federacion3 = false, error_federacion4 = false, error_federacion5 = false, error_federacion6 = false;
 
 	do {//bucle de datos
 		numero = cin.get();
@@ -25,34 +25,72 @@ int main () {
 				numero = cin.get();
 				if (numero >= '0' && numero <= '9'){
 					numero_real = numero_real * 10 + (numero - '0');
-				}else {
-					numero_real = -7346910;
+				}else if (numero != ' ' && numero != '\n'){
+					numero_real = -725774;
 				}
 			} while (numero != ' ' && numero != '\n');
-			if (j == 0){
+			if (numero_real == -725774){
+				error_simbolo = true;
+			}
+			if (numero_real == -725774 && j == 0){
+				error_equipos = true;
+			}else if (j == 0){
 				equipos = numero_real;
-			}else {
+			}else if(j == 1 && numero_real == -725774){
+				error_federacion1 = true;
+			}else if(j == 2 && numero_real == -725774){
+				error_federacion2 = true;
+			}else if(j == 3 && numero_real == -725774){
+				error_federacion3 = true;
+			}else if(j == 4 && numero_real == -725774){
+				error_federacion4 = true;
+			}else if(j == 5 && numero_real == -725774){
+				error_federacion5 = true;
+			}else if(j == 6 && numero_real == -725774){
+				error_federacion6 = true;
+			}else{
 				plazas = numero_real;
 				plazas_totales += plazas;
 			}
-			cout << "equipos " << equipos << " plazas " << plazas << endl;
 			j++;
 		}while ( numero != '\n');
 		repesca = equipos - plazas_totales;
-		if (equipos == -7346910){
-			cout << RED << "ERROR: Simbolos erroneos detectados en el número de equipos" << RESET << endl;
-		}
-		else if ((equipos < 1) || (equipos > 128)){
-			cout << RED << "ERROR: El número de equipos debe ser entre 1 y 128" << RESET << endl;
-		}
-		if (plazas_totales > equipos){
-			cout << RED << "ERROR: El número de plazas totales no puede ser mayor al número de equipos" << RESET << endl;
-		}
-		if (j != 7 ){
-			cout << RED << "ERROR: Número incorrrecto de federaciones" << RESET << endl;
+		if (error_simbolo){
+			cout << RED << "ERROR: Simbolos erroneos detectados en: ";
+			if (error_equipos){
+				cout << "el número de equipos ";
+			}
+			if (error_federacion1){
+				cout << "la federación UEFA ";
+			}
+			if (error_federacion2){
+				cout << "la federación AFC ";
+			}
+			if (error_federacion3){
+				cout << "la federación CAF ";
+			}
+			if (error_federacion4){
+				cout << "la federación CONCACAF ";
+			}
+			if (error_federacion5){
+				cout << "la federación CONMEBOL ";
+			}
+			if (error_federacion6){
+				cout << "la federación OFC ";
+			}
+			cout << RESET << endl;
+		}else if (equipos > 128){
+			cout << RED << "ERROR: Demasiados equipos" << RESET << endl;
+		}else if (equipos < 1){
+			cout << RED << "ERROR: No hay suficientes equipos" << RESET << endl;
+		}else if (j > 7 ){
+			cout << RED << "ERROR: Demasiadas federaciones" << RESET << endl;
 
-		}
-		if (j == 7 && plazas_totales < equipos && equipos > 1 && equipos < 128){
+		}else if (j < 7 ){
+			cout << RED << "ERROR: No hay suficientes federaciones" << RESET << endl;
+		}else if (plazas_totales > equipos){
+			cout << RED << "ERROR: Plazas superadas" << RESET << endl;
+		}else {
 		cout << GREEN << repesca << RESET << endl;
 		}
 		i++;
