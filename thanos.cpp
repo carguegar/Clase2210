79fa8685479abbe7179cd35919b0c466bedd2a64
carguegar <carlosgue.2024.alumn@cescristorey.com>
2024-12-17 14:06:22 +0100

    v0.3

--- thanos.cpp
+++ thanos.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main () {
 	int num_personas, num_casos, num_muertes, num_saltos, pos_stark, pos_peter;
+	bool stark_muerto = false, peter_muerto = false;
 
 	cin >> num_casos;
 	cin >> num_personas >> pos_stark >> pos_peter >> num_saltos;
@@ -16,8 +17,18 @@ int main () {
 		for (int j = num_saltos + 1; ; j += num_saltos + 1){//posición que mato
 			if (j > num_personas){
 				j = j - num_personas;
+			}else (if j == pos_stark){
+				stark_muerto = true;
+			}if (j == pos_peter){
+				peter_muerto = true;
 			}
 
 		}
+	}
+	if (stark_muerto && peter_muerto == false){
+		cout << "No quiero irme, Peter!" << endl;
+			
+	}else if (peter_muerto && stark_muerto == false){
+		cout << "No quiero irme, Sr. Stark!"
 	}
 }
